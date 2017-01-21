82814e2809469d748a558f9174291bb74a617589
AurelienCaille <aur-lien@hotmail.fr>
2017-01-21 13:24:47 +0100

    Update CarteDestination.py

--- CarteDestination.py
+++ CarteDestination.py
@@ -1,2 +1,8 @@
 class CarteDestination:
-    pass
+    def __init__(self, depart, arrive):
+        self.depart = depart
+        self.arrive = arrive
+        
+    def __repr__(self):
+        
+        return (self.depart + "--->" + self.arrive)
