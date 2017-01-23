5cdee2902669b3205b658a9cd014a7cfa3875d50
Caille Aurelien <aur-lien@hotmail.fr>
2017-01-24 00:11:37 +0100

    Les cartes wagon piochees sont affichees Modification commentaires selon pylint

--- Joueur.py
+++ Joueur.py
@@ -23,6 +23,9 @@ class Joueur(object):
         self.plateau_de_jeu = None
         self.couleur = couleur
 
+    def jouer(self):
+        """ Methode abstraite lancant le tour du Joueur """
+        print("Le joueur", self.couleur, "commence son tour")
 
     def calculer_score_finale(self):
         """ Calcule le score final du joueur selon les regles du jeu CF: ManuelDuJoueur"""
@@ -35,11 +38,16 @@ class Joueur(object):
         et complete les cartes manquantes sur le plateau de jeu
         """
 
-        #On ajoute les cartes a la mains
-        self.cartes_wagons.append(self.plateau_de_jeu.cartes_wagon_visibles.pop(indice1))
-        self.cartes_wagons.append(self.plateau_de_jeu.cartes_wagon_visibles.pop(indice2))
+        # On ajoute les cartes a la mains
+        nouvelle_carte_1 = self.plateau_de_jeu.cartes_wagon_visibles.pop(indice1)
+        nouvelle_carte_2 = self.plateau_de_jeu.cartes_wagon_visibles.pop(indice2)
 
-        #On revele deux nouvelles cartes
+        print("Les cartes piochees sont:", nouvelle_carte_1, nouvelle_carte_2)
+
+        self.cartes_wagons.append(nouvelle_carte_1)
+        self.cartes_wagons.append(nouvelle_carte_2)
+
+        # On revele deux nouvelles cartes
         self.plateau_de_jeu.cartes_wagon_visibles.append(\
             self.plateau_de_jeu.pioche_carte_wagon.piocher())
         self.plateau_de_jeu.cartes_wagon_visibles.append(\
@@ -50,6 +58,7 @@ class Joueur(object):
         pass
 
     def construire_route(self, depart, arrive, nombre_locomotive, couleur_desiree):
+
         """
         Construit une route entre deux stations selon les regles du jeu CF: ManuelDuJoueur
         Retourne True si la route est construite, False si elle n'a pas pu etre construite
@@ -58,14 +67,14 @@ class Joueur(object):
         longueur_route = None
         couleur_route = None
 
-        #On verifie que la route existe
+        # On verifie que la route existe
         if not depart in self.plateau_de_jeu.graphe.adjacency_list or not\
             arrive in self.plateau_de_jeu.graphe.adjacency_list[depart]:
 
             print("La route n'existe pas!!!")
             return False
 
-        #On recupere alors la longueur et couleur de la route
+        # On recupere alors la longueur et couleur de la route
         for edge in self.plateau_de_jeu.map.edges:
             if edge[0] == depart and edge[1] == arrive:
                 pass
@@ -74,12 +83,12 @@ class Joueur(object):
             print("La couleur de la route et la couleur desiree ne sont pas les memes !!!")
             return False
 
-        #On verifie que l'on a assez de wagon
+        # On verifie que l'on a assez de wagon
         if self.reserve_wagon < longueur_route:
             print("Le joueur n'a plus assez de wagon")
             return False
 
-        #On verifie que l'on a bien toutes les cartes
+        # On verifie que l'on a bien toutes les cartes
         if not len([carte_wagon \
         for carte_wagon in self.cartes_wagons \
         if carte_wagon.couleur == "Multicolore"]) >= nombre_locomotive:
@@ -94,7 +103,7 @@ class Joueur(object):
             print("Le joueur n'a pas assez de carte wagon de la bonne couleur")
             return False
 
-        #On construit la route et retire carte // wagon
+        # On construit la route et retire carte // wagon
         self.plateau_de_jeu.construction.add_an_edge(depart, arrive, self.couleur)
 
         self.reserve_wagon -= longueur_route
