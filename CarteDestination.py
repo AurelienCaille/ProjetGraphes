6eed27c99be7d267e30f8ce085329d4f358e7895
Caille Aurelien <aur-lien@hotmail.fr>
2017-01-25 11:36:37 +0100

    Ajout attribut point

--- CarteDestination.py
+++ CarteDestination.py
@@ -1,12 +1,14 @@
-class CarteDestination:
-    def __init__(self, depart, arrive):
+class CarteDestination(object):
+    """ Class representant une carte destination du jeu """
+    def __init__(self, depart, arrive, point):
         """
-        :attribut self.depart: def self.depart to depart 
+        :attribut self.depart: def self.depart to depart
         :attribut self.arrive: def self.depart to arrive
         """
         self.depart = depart
         self.arrive = arrive
-        
+        self.point = point
+
     def __repr__(self):
-        
+
         return (self.depart + "--->" + self.arrive)
