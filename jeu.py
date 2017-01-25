036546628fb9a0c9f6d444ba3ddcabd3e27afdc5
Caille Aurelien <aur-lien@hotmail.fr>
2017-01-25 10:25:34 +0100

    Ajout fichier jeu Permet de lancer une partie de maniere guidee

---
+++ jeu.py
@@ -0,0 +1,32 @@
+from PlateauDeJeu import PlateauDeJeu
+from Humain import Humain
+from Ordinateur import Ordinateur
+
+
+if __name__ == "__main__":
+
+    # On cree les deux joueurs
+    joueur_1 = Humain("Bleu")
+    joueur_2 = None
+
+    print("Les aventuriers du rail: initialisation de la partie")
+
+    choix_ordinateur = False
+
+    while choix_ordinateur is False:
+        input_ordinateur = input("Voulez vous inclure un Ordinateur: Oui(o) Non(n) ?")
+
+        if input_ordinateur == "o":
+            choix_ordinateur = True
+            joueur_2 = Ordinateur("Vert")
+        elif input_ordinateur == "n":
+            choix_ordinateur = True
+            joueur_2 = Humain("Rouge")
+        else:
+            print("Choix impossible!")
+
+    # On cree le plateau du jeu
+    plateau = PlateauDeJeu(joueur_1, joueur_2)
+
+    # On lance la partie
+    plateau.jouer()
